37c2e1399f284dabf2e3a6111bcd345a1ecb592f
Vivian <vivianhuang197@gmail.com>
2022-07-31 21:46:20 +0800

    updated index

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,16 @@
-console.log('Hello!');
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+$(document).ready(function () {
+
+  $('.hamburgerMenu').click(function (e) {
+    e.preventDefault();
+    $('.mobileMenu').slideToggle(600);
+  });
+
+  $('.mobileMenu li a').click(function () {
+
+    //收起來
+    $('.mobileMenu').slideUp(600);
+  });
+
+
 });
